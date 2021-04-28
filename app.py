f60b446ce70248f4be854f1dc026fe42d213d530
lvhaodong <1720866841@qq.com>
2021-04-28 23:24:46 +0800

    add index page

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask
-from flask import escape,url_for
+from flask import escape,url_for,render_template
 app=Flask(__name__)
 
+# 运行前输入 $env:FLASK_ENV = "development" 设置为调试模式
+# flask run 运行
 @app.route('/home')
 def hello():
     return '<h1>Hello Totoro!</h1><img src="http://helloflask.com/totoro.gif">'
@@ -18,3 +20,22 @@ def test_url_for():
     print(url_for('test_url_for'))
     print(url_for('test_url_for',num=2))
     return 'test page'
+
+@app.route('/')
+def index():
+    return render_template('index.html',name=name,movies=movies)
+
+
+name = 'Grey Li'
+movies = [
+    {'title': 'My Neighbor Totoro', 'year': '1988'},
+    {'title': 'Dead Poets Society', 'year': '1989'},
+    {'title': 'A Perfect World', 'year': '1993'},
+    {'title': 'Leon', 'year': '1994'},
+    {'title': 'Mahjong', 'year': '1996'},
+    {'title': 'Swallowtail Butterfly', 'year': '1996'},
+    {'title': 'King of Comedy', 'year': '1999'},
+    {'title': 'Devils on the Doorstep', 'year': '1999'},
+    {'title': 'WALL-E', 'year': '2008'},
+    {'title': 'The Pork of Music', 'year': '2012'},
+]
